895eb676d93187087b73aa3a9089d5546784ac77
Shiliu <67215344+Shiliu5@users.noreply.github.com>
2023-05-20 14:43:01 +0800

    Delete Pingtest.py

--- Pingtest.py
+++
@@ -1,9 +0,0 @@
-import ping3
-
-hostname = "www.google.com"
-response_time = ping3.ping(hostname)
-
-if response_time is not None:
-    print(f"Response time for {hostname}: {response_time} ms")
-else:
-    print(f"No response from {hostname}")

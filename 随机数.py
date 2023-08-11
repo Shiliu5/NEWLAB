e7f80098c0a4dcc0b05e4421bfc9ade204f82a62
Shiliu <67215344+Shiliu5@users.noreply.github.com>
2023-08-11 17:08:07 +0800

    Delete 随机数.py

--- 随机数.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def random_number_generator(min_value=1, max_value=100):
-    return random.randint(min_value, max_value)
-
-random_number = random_number_generator()
-print("随机数是:", random_number)
